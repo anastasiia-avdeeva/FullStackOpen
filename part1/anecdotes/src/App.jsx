b8e20d2fb9019dc01c8ed5387c7b0511b32a481b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-07 05:37:12 +0300

    create js file constants and move anecdotes array to it, create TextBtn component, write code for App component with anecdote and a button that when clicked displays random anecdote from the array

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { anecdotes } from "./constants/constants";
+import { TextBtn } from "./components/TextBtn";
+
 const App = () => {
-  return <div></div>;
+  const chooseRandom = () => Math.floor(Math.random() * anecdotes.length);
+  const [selected, setSelected] = useState(0);
+
+  const handleNextClick = () => {
+    console.log(selected);
+    setSelected(chooseRandom());
+  };
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <TextBtn text="next anecdote" onClick={handleNextClick} />
+    </div>
+  );
 };
 
 export default App;
