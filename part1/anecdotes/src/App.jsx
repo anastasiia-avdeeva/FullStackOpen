0124a5b7c72ec287504cfc27e680c770cfd7b065
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-07 05:53:16 +0300

    add vote state, paragraph that shows the number of votes for the anecdote and a button for voting for the anecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -5,14 +5,25 @@ import { TextBtn } from "./components/TextBtn";
 const App = () => {
   const chooseRandom = () => Math.floor(Math.random() * anecdotes.length);
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const handleNextClick = () => {
-    console.log(selected);
     setSelected(chooseRandom());
   };
+
+  const handleVoteClick = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>
+        has {votes[selected]} {votes[selected] === 1 ? "vote" : "votes"}{" "}
+      </p>
+      <TextBtn text="vote" onClick={handleVoteClick} />
       <TextBtn text="next anecdote" onClick={handleNextClick} />
     </div>
   );
