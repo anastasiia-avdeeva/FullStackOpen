4626d836d8c1a0f0e9af2c36f306d8dee9719e1d
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-11 06:25:25 +0300

    write function to count total number of exercises to the Course component and use Total component to display total number of exercises. Finish exercise 2.2

--- part2/courseInfo2/src/components/Course.jsx
+++ part2/courseInfo2/src/components/Course.jsx
@@ -1,11 +1,19 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
+import { Total } from "./Total";
 
 export const Course = ({ course }) => {
+  const countTotalExercises = () => {
+    return course.parts.reduce(
+      (accumulator, part) => (accumulator += part.exercises),
+      0
+    );
+  };
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total total={countTotalExercises()} />
     </div>
   );
 };
--- part2/courseInfo2/src/components/Total.jsx
+++ part2/courseInfo2/src/components/Total.jsx
@@ -1,1 +1,7 @@
-export const Total = ({ total }) => <p>Number of exercises {total}</p>;
+export const Total = ({ total }) => (
+  <p>
+    <b>
+      total of {total} {total === 1 ? "exercise" : "exercises"}
+    </b>
+  </p>
+);
