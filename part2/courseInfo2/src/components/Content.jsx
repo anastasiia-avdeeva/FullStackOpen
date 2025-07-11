e0e264feaa4d6a2209553a4dfddc13fb62465e30
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-11 06:15:49 +0300

    move different components into a separate files, create Course component and use it in App.jsx, use map method in Content. Finish exercise 2.1

---
+++ part2/courseInfo2/src/components/Content.jsx
@@ -0,0 +1,9 @@
+import { Part } from "./Part.jsx";
+
+export const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
