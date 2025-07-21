56625f563ef2da8eeba1e6fa43751d93c3fcf97e
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-22 00:52:05 +0300

    delete unused code

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,5 +1,4 @@
 export const Notification = ({ className, msg }) => {
-  if (!msg) return null;
   let classNames = "notification";
   if (className) classNames += ` ${className}`;
   return <p className={classNames}>{msg}</p>;
