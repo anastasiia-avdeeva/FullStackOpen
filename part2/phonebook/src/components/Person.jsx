bbb42ec65eaa48b7ed49b75560f55ec3b508066c
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-21 21:14:49 +0300

    small adjustments to styles

--- part2/phonebook/src/components/Person.jsx
+++ part2/phonebook/src/components/Person.jsx
@@ -2,7 +2,9 @@ import { Button } from "./button";
 
 export const Person = ({ person, onDelete }) => (
   <li className="person">
-    {person.name} {person.number}{" "}
+    <span className="person-info">
+      {person.name} {person.number}{" "}
+    </span>
     <Button text="delete" onClick={onDelete} className="btn del-btn" />
   </li>
 );
