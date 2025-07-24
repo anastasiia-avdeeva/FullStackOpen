b890583faeedb6fd63c86712c92c7ea48fa95a06
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-24 22:43:15 +0300

    finish exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import countriesService from "./services/countries";
+import { Countries } from "./components/Countries";
+import { SearchCountries } from "./components/SearchCountries";
 
 function App() {
-  return <></>;
+  const [allCountries, setAllCountries] = useState([]);
+  const [userSearchValue, setUserSearchValue] = useState("");
+  // const [fetchError, setFetchError] = useState("");
+
+  const countriesShown = allCountries.filter((country) =>
+    country.name.common.toLowerCase().includes(userSearchValue.toLowerCase())
+  );
+
+  useEffect(() => {
+    countriesService.fetchAllCountries().then((newCountries) => {
+      setAllCountries(newCountries);
+    });
+  }, []);
+
+  const handleInputChange = (evt) => {
+    const inputValue = evt.target.value;
+    setUserSearchValue(inputValue);
+  };
+  return (
+    <>
+      <h1>Countries</h1>
+      <SearchCountries value={userSearchValue} onChange={handleInputChange} />
+      <Countries countries={countriesShown} />
+    </>
+  );
 }
 
 export default App;
