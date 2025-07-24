1fe8c0bc1860dae2b34de56e1496bb0384c1195a
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-24 21:05:56 +0300

    create file with function to fecth all countries from the api

---
+++ part2/countries/src/services/countries.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/";
+const allCountriesURL = `${baseUrl}all`;
+const countryURL = `${baseUrl}name/`;
+
+const fetchAllCountries = () => {
+  const request = axios.get(allCountriesURL);
+  return request.then((response) => response.data);
+};
+
+export default { fetchAllCountries };
