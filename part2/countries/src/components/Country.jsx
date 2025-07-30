88acbd58069fa99e3fc3b724f4d50462997f7064
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-30 22:49:53 +0300

    move weather states and effect from country component to weather component

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,26 +1,13 @@
-import { useState, useEffect } from "react";
-import weatherService from "../services/weather";
 import { CountryBasics } from "./CountryBasics";
 import { Weather } from "./Weather";
 
 export const Country = ({ info }) => {
-  const [weather, setWeather] = useState(null);
-  //   const [error, setError] = use(null);
-
   const langArray = Object.values(info.languages);
-
-  useEffect(() => {
-    weatherService
-      .getCapitalWeather(info.capital[0])
-      .then((weatherInfo) => setWeather(weatherInfo));
-  }, [info.capital]);
 
   return (
     <article>
       <CountryBasics info={info} languages={langArray} />
-      {weather && (
-        <Weather weather={weather} weatherIconInfo={weather.weather[0]} />
-      )}
+      <Weather city={info.capital[0]} />
     </article>
   );
 };
--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from "react";
+import weatherService from "../services/weather";
+import { ErrorMsg } from "./ErrorMsg";
 import { ImageContainer } from "./ImageContainer";
 
-export const Weather = ({ weather, weatherIconInfo }) => {
-  const iconSrc = `https://openweathermap.org/img/wn/${weatherIconInfo.icon}@2x.png`;
-  const iconAlt = `${weatherIconInfo.description} icon`;
+export const Weather = ({ city }) => {
+  const [weather, setWeather] = useState(null);
+  const [weatherError, setWeatherError] = useState(null);
+
+  useEffect(() => {
+    weatherService
+      .getCapitalWeather(city)
+      .then((weatherInfo) => setWeather(weatherInfo))
+      .catch((error) => {
+        console.log(`Cannot get weather info for ${city}`, error);
+        setWeatherError(
+          "Cannot get weather from the server 😞 Please, try again later!"
+        );
+      });
+  }, [city]);
+
+  const iconInfo = weather?.weather?.[0];
+  const iconSrc = iconInfo
+    ? `https://openweathermap.org/img/wn/${iconInfo.icon}@2x.png`
+    : null;
+  const iconAlt = iconInfo ? `${iconInfo.description} icon` : "weather icon";
+
   return (
     <aside>
-      <h4>Weather in {weather.name}</h4>
-      <p>Temperature {weather.main.temp} Celsius</p>
-      <ImageContainer imgSrc={iconSrc} alt={iconAlt} />
-      <p>Wind {weather.wind.speed} m/s</p>
+      <h4>Weather in {city}</h4>
+      {weatherError && <ErrorMsg text={weatherError} />}
+      {weather && (
+        <>
+          <p>Temperature {weather.main.temp} Celsius</p>
+          {iconSrc && <ImageContainer imgSrc={iconSrc} alt={iconAlt} />}
+          <p>Wind {weather.wind.speed} m/s</p>
+        </>
+      )}
     </aside>
   );
 };
