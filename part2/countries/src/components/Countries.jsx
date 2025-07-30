7b5ce51f0414643cc8e6b1f0f2c19bb91c06b131
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-30 21:50:01 +0300

    add useEffect for handling situations when user change input while being on the page of the country. Refactor code

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Country } from "./Country";
 import { ErrorMsg } from "./ErrorMsg";
 
-export const Countries = ({ countries }) => {
+export const Countries = ({ countries, searchValue }) => {
   const [selectedCountry, setSelectedCountry] = useState(null);
 
-  const handleShowBtnClick = (cca3) =>
-    setSelectedCountry(countries.find((country) => country.cca3 === cca3));
+  useEffect(() => {
+    setSelectedCountry(null);
+  }, [searchValue]);
+
+  const handleShowBtnClick = (country) => setSelectedCountry(country);
 
   const handleBackBtnClick = () => setSelectedCountry(null);
 
@@ -19,32 +22,33 @@ export const Countries = ({ countries }) => {
     );
   }
 
-  let errorMsg = "";
+  let content = null;
 
   if (countries.length === 0) {
-    errorMsg = "No country matches this search request 😞";
+    content = <ErrorMsg text={"No country matches this search request 😞"} />;
+  } else if (countries.length === 1) {
+    content = <Country info={countries[0]} />;
   } else if (countries.length > 10) {
-    errorMsg = "Too many matches. Please, specify search request!";
+    content = (
+      <ErrorMsg text={"Too many matches. Please, specify search request!"} />
+    );
+  } else {
+    content = (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.cca3}>
+            {country.name.common}{" "}
+            <button onClick={() => handleShowBtnClick(country)}>Show</button>
+          </li>
+        ))}
+      </ul>
+    );
   }
+
   return (
     <div>
-      <h2>Results:</h2>
-      {errorMsg ? (
-        <ErrorMsg text={errorMsg} />
-      ) : countries.length > 1 ? (
-        <ul>
-          {countries.map((country) => (
-            <li key={country.cca3}>
-              {country.name.common}{" "}
-              <button onClick={() => handleShowBtnClick(country.cca3)}>
-                Show
-              </button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <Country info={countries[0]} />
-      )}
+      <h2>Search results:</h2>
+      {content}
     </div>
   );
 };
