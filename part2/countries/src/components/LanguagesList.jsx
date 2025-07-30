894b1a2e0b0610f8b0fa3ce55a45cc7d869de4e7
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-07-30 23:04:36 +0300

    code refactoring

--- part2/countries/src/components/LanguagesList.jsx
+++ part2/countries/src/components/LanguagesList.jsx
@@ -1,23 +1,16 @@
 export const LanguagesList = ({ languages }) => {
-  let content;
-
-  if (languages.length === 0) {
-    content = <p>Unknown</p>;
-  } else if (languages.length === 1) {
-    content = <p>{languages[0]}</p>;
-  } else {
-    content = (
-      <ul>
-        {languages.map((lang) => (
-          <li key={lang}>{lang}</li>
-        ))}
-      </ul>
-    );
-  }
   return (
     <div>
       <h4>{languages.length < 2 ? "Language:" : "Languages:"}</h4>
-      {content}
+      {languages.length === 0 ? (
+        <p>Unknown</p>
+      ) : (
+        <ul>
+          {languages.map((lang) => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
