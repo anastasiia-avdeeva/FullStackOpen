6c05f0cc8a07db94516959ad2e05e6c5c73f9e72
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-08-24 22:52:17 +0300

    extract environmental vars in separate module utils/config.js, import it to index.js,  made test requests

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,12 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(express.json());
 
@@ -14,23 +13,8 @@ app.get("/", (request, response) => {
   response.send("<h1>Blog List</h1>");
 });
 
-// app.get("/api/blogs", (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs);
-//   });
-// });
-
-// app.post("/api/blogs", (request, response) => {
-//   const blog = new Blog(request.body);
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result);
-//   });
-// });
-
 app.use("/api/blogs", blogsRouter);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
